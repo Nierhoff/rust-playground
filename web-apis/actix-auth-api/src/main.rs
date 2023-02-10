eb9018b223f7a85d68832b514102d7771a72af33
Anders Nierhoff <Anders.Nierhoff@gmail.com>
2023-02-10 13:35:52 +0100

    Adding a authentication api

---
+++ web-apis/actix-auth-api/src/main.rs
@@ -0,0 +1,33 @@
+use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
+use log::*;
+use log4rs;
+use tracing_actix_web::TracingLogger;
+use utoipa::OpenApi;
+use utoipa_swagger_ui::SwaggerUi;
+
+#[derive(OpenApi)]
+#[openapi()]
+struct ApiDoc;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    log4rs::init_file("logging_config.yaml", Default::default()).unwrap();
+    let openapi = ApiDoc::openapi();
+    HttpServer::new(move || {
+        App::new().wrap(TracingLogger::default()).service(
+            SwaggerUi::new("/swagger-ui/{_:.*}").url("/api-doc/openapi.json", openapi.clone()),
+        )
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn it_works() {
+        assert_eq!(true, true);
+    }
+}
---
+++ web-apis/actix-auth-api/tests/main.rs
@@ -0,0 +1,9 @@
+#[cfg(test)]
+mod tests {
+    use actix_web::test;
+
+    #[actix_web::test]
+    async fn test_index_post() {
+        assert!(true);
+    }
+}
